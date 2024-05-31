5481299d5c59c8ab239fbae8449cd38bb7d11e26
Sveta2507 <dnsdns.dn@googlemail.com>
2024-05-31 15:49:24 +0300

    added tests and .env

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe, Category
 
-# Create your tests here.
+class RecipeViewsTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+        self.recipe = Recipe.objects.create(
+            title='Test Recipe',
+            description='Test Description',
+            instructions='Test Instructions',
+            ingredients='Test Ingredients',
+            category=self.category
+        )
+
+    def test_recipe_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipe/main.html')
+
+    def test_category_list_view(self):
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipe/category_list.html')
--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,3 @@
-# recipe/views.py
 from django.shortcuts import render
 from django.db.models import Count
 from .models import Recipe, Category
