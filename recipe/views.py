30246f116fec83be51225e9e524e27eb39b6d813
Sveta2507 <dnsdns.dn@googlemail.com>
2024-05-31 15:33:21 +0300

    corrupted migration fix

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,12 @@
+# recipe/views.py
+from django.shortcuts import render
+from django.db.models import Count
+from .models import Recipe, Category
 
+def main(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:5]
+    return render(request, 'recipe/main.html', {'recipes': latest_recipes})
+
+def category_list(request):
+    categories = Category.objects.annotate(recipe_count=Count('recipes'))
+    return render(request, 'recipe/category_list.html', {'categories': categories})
